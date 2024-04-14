7cfbe04dd34c308ad67e4f68794ea6bd4a64d17d
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-14 11:53:18 +0300

    run original tests to make sure you've not damaged codebase

--- models/base_model.py
+++ models/base_model.py
@@ -18,54 +18,30 @@ class BaseModel:
     """
     A base class for all hbnb models
     """
-    id = Column(String(60), nullable=False, unique=True, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        id = Column(String(60), nullable=False, unique=True, primary_key=True)
+        created_at = Column(DateTime,
+                            nullable=False,
+                            default=datetime.utcnow())
+        updated_at = Column(DateTime,
+                            nullable=False,
+                            default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model
-        """
-
-        if len(kwargs) == 0:
+        """Instatntiates a new model"""
+        if not kwargs:
+            from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
         else:
-            if not kwargs.get('id'):
-                self.id = str(uuid.uuid4())
-            if kwargs.get("updated_at"):
-                kwargs['updated_at'] = datetime.strptime(
-                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                kwargs['updated_at'] = datetime.utcnow()
-            if kwargs.get("created_at"):
-                kwargs['created_at'] = datetime.strptime(
-                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                kwargs['created_at'] = datetime.utcnow()
-            for key, val in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, val)
-            """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
-            else:
-                self.created_at = datetime.utcnow()
-            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
-            else:
-                self.updated_at = datetime.utcnow()
-            if kwargs.get("id", None) is None:
-                self.id = str(uuid.uuid4())
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = self.created_at
-            """
-
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
