5fd9430f73cc2eb24a731926ab722b56d63ced70
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-14 14:50:59 +0300

    delete casscade for orphan

--- models/user.py
+++ models/user.py
@@ -10,24 +10,21 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    #    if getenv("HBNB_TYPE_STORAGE") == 'db':
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place",
-                          backref="user",
-                          cascade="all, delete-orphan")
-    reviews = relationship("Review",
-                           backref="user",
-                           cascade="all, delete-orphan")
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place",
+                              backref="user",
+                              cascade="delete")
+        reviews = relationship("Review",
+                               backref="user",
+                               cascade="delete")
 
-
-"""
     else:
         email = ''
         password = ''
         first_name = ''
         last_name = ''
-"""
