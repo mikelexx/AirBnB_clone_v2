afce623fb6e446e528f9acc8f5635246c089b1c0
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-14 13:30:01 +0300

    added import statements

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.state import State
+
 import json
 
 
