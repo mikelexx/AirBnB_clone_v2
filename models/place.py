78ff440ce978af6cc439af28fed68845bf2fed40
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-15 09:59:37 +0300

    multiple places can appear on relationship but amenity is unique

--- models/place.py
+++ models/place.py
@@ -7,12 +7,14 @@ from sqlalchemy import Table, Column, Float, String, Integer, ForeignKey
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
     place_amenity = Table(
-        'place_amenity', Base.metadata,
-        Column('place_id',
-               String(60),
-               ForeignKey('places.id'),
-               primary_key=True,
-               nullable=False),
+        'place_amenity',
+        Base.metadata,
+        Column(
+            'place_id',
+            String(60),
+            ForeignKey('places.id'),
+            #   primary_key=True,
+            nullable=False),
         Column('amenity_id',
                String(60),
                ForeignKey('amenities.id'),
