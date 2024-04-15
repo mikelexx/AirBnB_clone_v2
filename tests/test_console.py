24c9296dd027bc4149a31d8ac08eadb12768d7c4
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-15 08:49:07 +0300

    fix pep8 formatting

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,6 +5,8 @@ import sys
 from io import StringIO
 from unittest.mock import create_autospec
 from console import HBNBCommand
+
+
 class TestHBNBCommand(unittest.TestCase):
     """
     THis class tests the console module
@@ -14,11 +16,11 @@ class TestHBNBCommand(unittest.TestCase):
     def setUp(self):
         """ sets up resources for redirection of output from console
         """
-        #save origninal value of sys.out to restore later
+        # save origninal value of sys.out to restore later
         self.initial_std_out = sys.stdout
         # for capturing information that would be normally printed on console
         self.captured_output = StringIO()
-        #redirect standard output to stringIo()
+        # redirect standard output to stringIo()
         sys.stdout = self.captured_output
 
     @classmethod
@@ -26,7 +28,8 @@ class TestHBNBCommand(unittest.TestCase):
         """ restore output to its initial state"""
         sys.stdout = self.initial_std_out
 
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') =='db', "works only for file storage")
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') == 'db', "works only for file storage")
     def test_all_command(self):
         """
         Tests if all command works
