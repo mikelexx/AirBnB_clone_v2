37ab184629500e218d7ae9930c90ee1a9f1994df
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-15 09:37:18 +0300

    uncomment an attribut i had commented for debugging purposes

--- models/amenity.py
+++ models/amenity.py
@@ -13,11 +13,9 @@ class Amenity(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
-        """
         from models.place import place_amenity
         place_amenities = relationship("Place",
                                        secondary=place_amenity,
                                        back_populates="amenities")
-        """
     else:
         name = ""
