29e29f017c2607e1cfbbd35ce31a6336b6d8171d
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-15 10:03:18 +0300

    remove primary keys for many to many relationship table

--- models/place.py
+++ models/place.py
@@ -13,12 +13,12 @@ if getenv("HBNB_TYPE_STORAGE") == "db":
             'place_id',
             String(60),
             ForeignKey('places.id'),
-            #   primary_key=True,
+            # primary_key=True,
             nullable=False),
         Column('amenity_id',
                String(60),
                ForeignKey('amenities.id'),
-               primary_key=True,
+               # primary_key=True,
                nullable=False))
 
 
