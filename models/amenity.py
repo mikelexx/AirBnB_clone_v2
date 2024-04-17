eeada2ae72ca23ab84400c1fd80e6a4d77d13f8f
qlibane <qjirmo@gmail.com>
2024-04-17 06:24:23 -0700

    changes to models/

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
-from models.base_model import BaseModel, Base
-from os import getenv
-from sqlalchemy import Column, String
+"""Defines the Amenity class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
+    """Represents an Amenity for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Amenities.
+        name (sqlalchemy String): The amenity name.
+        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
     """
-    describes features of a place
-    """
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = "amenities"
-        name = Column(String(128), nullable=False)
-        from models.place import place_amenity
-        place_amenities = relationship("Place",
-                                       secondary=place_amenity,
-                                       back_populates="amenities")
-    else:
-        name = ""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
+
--- models/review.py
+++ models/review.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel, Base
+"""Defines the Review class."""
+from models.base_model import Base
+from models.base_model import BaseModel
 from sqlalchemy import Column
-from sqlalchemy.orm import relationship
-from sqlalchemy import Integer, String
 from sqlalchemy import ForeignKey
-import os
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel, Base):
-    """ Review classto store review information """
+    """Represents a review for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table reviews.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Reviews.
+        text (sqlalchemy String): The review description.
+        place_id (sqlalchemy String): The review's place id.
+        user_id (sqlalchemy String): The review's user id.
+    """
     __tablename__ = "reviews"
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+
