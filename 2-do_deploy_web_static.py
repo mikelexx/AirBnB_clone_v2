301630758ae7764d1b6edc046dc03308a5a17eae
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-05-04 19:04:09 +0300

    print alert for success

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -36,6 +36,7 @@ def do_deploy(archive_path):
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
             format(arch_file_no_ext))
+        print('New version deployed!')
     except Exception as e:
         print("Erros occured: ", e)
         return False
