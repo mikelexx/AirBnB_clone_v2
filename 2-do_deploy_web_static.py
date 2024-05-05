fde78933d465e31f9685cf0be4a6b079f3a480f0
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-05-05 07:26:30 +0300

    try debugging with a temporary file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,28 +14,29 @@ def do_deploy(archive_path):
     """
     distributes an archive to your web servers, using the function do_deploy:
     """
-    if not os.path.exists(archive_path):
-        return False
-    try:
+    def do_deploy(archive_path):
+    temp = archive_path.split(".tgz")
+    arch_file_path = "".join(temp)
+    b_list = arch_file_path.split("versions/")
+    arch_file = "".join(b_list)
+    c_list = archive_path.split("versions/")
+    archive_wo_ver = "".join(c_list)
+    if archive_path:
         put(archive_path, '/tmp/')
-        parent_path, arch_file = os.path.split(archive_path)
-        arch_file_no_ext = arch_file.split(".tgz")[0]
-        run("mkdir -p /data/web_static/releases/{}/".format(
-            arch_file_no_ext))
-        run("tar -xzvf /tmp/{} -C /data/web_static/releases/{}/".format(
-            arch_file, arch_file_no_ext))
-        # decompresed files will begin with foldername/filename,
-        # but we want to keep only the file name
+        run("mkdir -p /data/web_static/releases/{}/".
+            format(arch_file))
+        run("tar -zxf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(archive_wo_ver, arch_file))
+        run("rm -r /tmp/{}".format(archive_wo_ver))
         run("mv /data/web_static/releases/{}/web_static/*\
-                /data/web_static/releases/{}/"
-            .format(arch_file_no_ext, arch_file_no_ext))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(
-            arch_file_no_ext))
-        run("rm -r /tmp/{}".format(arch_file))
+        /data/web_static/releases/{}/".format(arch_file,
+                                              arch_file))
+        run("rm -rf /data/web_static/releases/{}/web_static".
+            format(arch_file))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-            format(arch_file_no_ext))
-        print('New version deployed!')
-    except Exception as e:
+            format(arch_file))
+        print("New version deployed!")
+        return True
+    else:
         return False
-    return True
