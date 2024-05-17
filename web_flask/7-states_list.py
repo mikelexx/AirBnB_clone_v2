31c7f51bf10262c39d7e8179a5b029eb4d462016
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-05-17 15:50:10 +0300

    8.List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask application.
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def close_sqlalchemy_sessions(exc):
+    storage.close()
+
+
+@app.route("/states_list")
+def states_list():
+    """
+    returns html page for state objects sorted by name [A-Z].
+    """
+    states = storage.all(State)
+    states_list = []
+    for key, val in states.items():
+        states_list.append(val)
+    return render_template('7-states_list.html', states=states_list)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
