6346eb4eae763179cd7815926ebed1252ceae6db
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-05-17 11:34:12 +0300

    1.HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    """
+    Return: a welcome message
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """
+    Return: welcome message
+    """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
