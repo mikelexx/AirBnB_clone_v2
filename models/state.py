593a9df3cc47eaa6c917f439ab2a58fc4fc6a158
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-05-17 14:20:03 +0300

    fix pycodestyle

--- models/state.py
+++ models/state.py
@@ -22,9 +22,10 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
+    cities = relationship("City", backref="state", cascade="delete")
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
+
         @property
         def cities(self):
             """Get a list of all related City objects."""
@@ -33,4 +34,3 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
-
